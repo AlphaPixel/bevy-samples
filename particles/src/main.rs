a58a69b813ed19ac9c1997f5ccd35d829107a288
John W. Terrell <john@coolpeoplenetworks.com>
2023-12-26 11:03:19 -0800

    Clean up and comments.

--- particles/src/main.rs
+++ particles/src/main.rs
@@ -5,110 +5,135 @@ use std::time::{Duration, Instant};
 
 use bevy::diagnostic::FrameTimeDiagnosticsPlugin;
 
+// FPS counter module
 mod fps;
-use fps::{setup_fps_counter, fps_text_update_system, fps_counter_showhide};
+use fps::{fps_counter_showhide, fps_text_update_system, setup_fps_counter};
 
-const RADIUS: f32 = 0.2;
-const SPAWN_COUNT: usize = 30;
-const PARTICLE_EXPIRE_TIME_SECS: u64 = 10;
-const PARTICLE_RESPAWN_TIME_MS: u64 = 100;
-const CUBE_SIZE: f32 = 3.0;
-const INITIAL_VELOCITY: f32 = 1.81;
+// Compile time constants
+const PARTICLE_RADIUS: f32 = 0.2;
+const SPAWN_COUNT: usize = 30; // Number of particles to spawn when it's time to do so.
+const PARTICLE_EXPIRE_TIME_SECS: u64 = 10; // Number of seconds until each particle despawns.
+const PARTICLE_RESPAWN_TIME_MS: u64 = 100; // How often (in milliseconds) to wait until spawning more particles.
+const MAX_SPAWN_OFFSET: f32 = 3.0; // Max offset (in X, Z) of new particle location.
+const INITIAL_VELOCITY: f32 = 2.0; // Initial velocity vector magnitude of new particles.
+const GROUND_RADIUS: f32 = 10.0; // The "radius" of the ground plane.
 
 fn main() {
+    // Create the bevy 'app' and add all of the plugins/systems.
     App::new()
-    .add_plugins(FrameTimeDiagnosticsPlugin::default())    
-    .add_plugins(DefaultPlugins)
-    .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
-    .add_systems(Startup, setup)
-    .add_systems(Update, (spawn_particles, update_particles))
-    .add_systems(Update, bevy::window::close_on_esc)
-    // FPS display
-    .add_systems(Startup, setup_fps_counter)
-    .add_systems(Update, (
-        fps_text_update_system,
-        fps_counter_showhide,
-    ))
-    //
-    .run();
+        .add_plugins(DefaultPlugins)
+        .add_plugins(FrameTimeDiagnosticsPlugin{})
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+        .add_systems(Startup, setup)
+        .add_systems(Update, (spawn_particles, despawn_particles))
+        .add_systems(Update, bevy::window::close_on_esc)
+        // FPS display
+        .add_systems(Startup, setup_fps_counter)
+        .add_systems(Update, (fps_text_update_system, fps_counter_showhide))
+        //
+        .run();
 }
 
+// ParticleMarker - this component marks an entity as a particle.  Used for querying inside systems.
 #[derive(Component)]
 struct ParticleMarker;
 
+// ExpireTime - a component that denotes the time an entity should live before despawning.
 #[derive(Component)]
 struct ExpireTime(Instant);
-
 impl Default for ExpireTime {
     fn default() -> Self {
         ExpireTime(Instant::now())
     }
 }
 
+// Configuration - global resource containing system wide data.
 #[derive(Resource)]
 struct Configuration {
+    // The mesh for the particle.  Created once at setup and reused for all subsequent particles.
     sphere_mesh: Handle<Mesh>,
+    // The material for the particle.  Created once at setup and reused for all subsequent particles.
+    particle_material: Handle<StandardMaterial>,
+    // Used to determine how much time should elapse before spawning new particles.
     spawn_delta: Duration,
 }
 
+// Particle - A bundle (bevy-speak) containing the components that define a particle.
 #[derive(Bundle)]
 struct Particle {
+    // When should this particle expire (despawn)
     expire_time: ExpireTime,
+    // Marker denoting this entity is a particle
     marker: ParticleMarker,
-
+    // Particle's velocity vector
     velocity: Velocity,
-
+    // Particles geometry
     geometry: PbrBundle,
 }
 
+// setup - a setup system that creates global data and spawns fixed/static entities (camera, lights, ground, etc.)
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    // Create the mesh the particles will use
+    // Create the material the particles will use (this will be added to the configuration
+    // resource for later use)
+    let particle_material = materials.add(StandardMaterial {
+        base_color: Color::hex("#ff6060").unwrap(),
+        metallic: 1.0,
+        perceptual_roughness: 0.5,
+        ..default()
+    });
+
+    // Create the mesh the particles will use (this will be added to the configuration resource
+    // for later use)
     let sphere_mesh = meshes.add(
         Mesh::try_from(shape::Icosphere {
-            radius: RADIUS,
+            radius: PARTICLE_RADIUS,
             ..default()
         })
         .unwrap(),
     );
 
+    // Add the configuration resource to the world.
     commands.insert_resource(Configuration {
         sphere_mesh,
+        particle_material,
         spawn_delta: Duration::from_millis(PARTICLE_RESPAWN_TIME_MS),
     });
 
-    // ground
-    let ground_boundary = &[
-        Vec3::new(128.0, 0., 128.0),
-        Vec3::new(128.0, 0., -128.0),
-        Vec3::new(-128.0, 0., -128.0),
-        Vec3::new(-128.0, 0., 128.0),
+    // Create the ground entity
+    {
+        // Define the ground's boundary.  The will be given to the
+        // ground's collider so it interacts with the physics engine)
+        let ground_boundary = &[
+            Vec3::new(GROUND_RADIUS, 0., GROUND_RADIUS),
+            Vec3::new(GROUND_RADIUS, 0., -GROUND_RADIUS),
+            Vec3::new(-GROUND_RADIUS, 0., -GROUND_RADIUS),
+            Vec3::new(-GROUND_RADIUS, 0., GROUND_RADIUS),
+            Vec3::new(GROUND_RADIUS, -10.0, GROUND_RADIUS),
+            Vec3::new(GROUND_RADIUS, -10.0, -GROUND_RADIUS),
+            Vec3::new(-GROUND_RADIUS, -10.0, -GROUND_RADIUS),
+            Vec3::new(-GROUND_RADIUS, -10.0, GROUND_RADIUS),
+        ];
 
-        Vec3::new(128.0, -10.0, 128.0),
-        Vec3::new(128.0, -10.0, -128.0),
-        Vec3::new(-128.0, -10.0, -128.0),
-        Vec3::new(-128.0, -10.0, 128.0),
-    ];
+        // Spawn the ground plane - then insert the physics type and collider.
+        commands
+            .spawn(PbrBundle {
+                mesh: meshes.add(Mesh::from(shape::Plane {
+                    size: GROUND_RADIUS * 2.0,
+                    subdivisions: 16,
+                })),
+                material: materials.add(Color::rgb(0.4, 0.4, 0.4).into()),
+                transform: Transform::from_translation(Vec3::Y / 2.0),
+                ..Default::default()
+            })
+            .insert(RigidBody::Fixed)
+            .insert(Collider::convex_hull(ground_boundary).unwrap());
+    }
 
-    commands
-    // Spawn the ground plane
-    .spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Plane { 
-            size: 128.0,
-            subdivisions: 16,
-         }
-        )),
-        material: materials.add(Color::rgb(0.4, 0.4, 0.4).into()),
-        transform: Transform::from_translation(Vec3::Y / 2.0),
-        ..Default::default()
-    })
-    .insert(RigidBody::Fixed)
-    .insert(Collider::convex_hull(ground_boundary).unwrap());
-
-    // light
+    // Spawn a simple point light
     commands.spawn(PointLightBundle {
         transform: Transform::from_xyz(50.0, 50.0, 0.0),
         point_light: PointLight {
@@ -119,23 +144,18 @@ fn setup(
         ..default()
     });
 
-    // camera
-    commands.spawn(
-        Camera3dBundle {
-            transform: Transform::from_xyz(20.0, 20.0, 20.0).looking_at(Vec3::default(), Vec3::Y),
-            projection: PerspectiveProjection {
-                ..default()
-            }
-            .into(),
-            ..default()
-        }
-    );
+    // Spawn a simple perspective camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(20.0, 20.0, 20.0).looking_at(Vec3::default(), Vec3::Y),
+        projection: PerspectiveProjection { ..default() }.into(),
+        ..default()
+    });
 }
 
+// spawn_particle - an 'update' system that spawns new particles if it's time to do so.
 fn spawn_particles(
     configuration: Res<Configuration>,
     mut next_spawn_deadline: Local<ExpireTime>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
     mut commands: Commands,
 ) {
     // If it's time to spawn more particles...
@@ -154,35 +174,33 @@ fn spawn_particles(
             // Create a random vector that will contain the initial starting position
             // of the particle.
             let x = 1.0 + random::<f32>() * 2.0;
-            let y = CUBE_SIZE + 1.0 + random::<f32>() * 1.0;
+            let y = MAX_SPAWN_OFFSET + 1.0 + random::<f32>() * 1.0;
             let z = 1.0 + random::<f32>() * 2.0;
 
             // Spawn the particle using our Particle bundle struct.
-            commands.spawn(Particle {
-                expire_time: ExpireTime(Instant::now() + Duration::from_secs(PARTICLE_EXPIRE_TIME_SECS)),
-                marker: ParticleMarker {},
-                velocity: Velocity {
-                    linvel: v,
-                    angvel: Vec3::ZERO,
-                },
+            commands
+                .spawn(Particle {
+                    expire_time: ExpireTime(
+                        Instant::now() + Duration::from_secs(PARTICLE_EXPIRE_TIME_SECS),
+                    ),
+                    marker: ParticleMarker {},
+                    velocity: Velocity {
+                        linvel: v,
+                        angvel: Vec3::ZERO,
+                    },
 
-                // Set up the PBR bundle for the geometry that represents the particle (a simple sphere)
-                geometry: PbrBundle {
-                    mesh: configuration.sphere_mesh.clone(),
-                    transform: Transform::from_translation(Vec3::new(x, y, z)),
-                    material: materials.add(StandardMaterial {
-                        base_color: Color::hex("#ffd891").unwrap(),
-                        metallic: 1.0,
-                        perceptual_roughness: 0.5,
+                    // Set up the PBR bundle for the geometry that represents the particle (a simple sphere)
+                    geometry: PbrBundle {
+                        mesh: configuration.sphere_mesh.clone(),
+                        transform: Transform::from_translation(Vec3::new(x, y, z)),
+                        material: configuration.particle_material.clone(),
                         ..default()
-                    }),
-                    ..default()
-                }
-            })
-            // Insert a dynamic rigid body component for the particle
-            .insert(RigidBody::Dynamic)
-            // Insert a collider component for the particle
-            .insert(Collider::ball(RADIUS));
+                    },
+                })
+                // Insert a dynamic rigid body component for the particle
+                .insert(RigidBody::Dynamic)
+                // Insert a collider component for the particle
+                .insert(Collider::ball(PARTICLE_RADIUS));
         }
 
         // Udpate the deadline for the next round of particle spawns.
@@ -190,9 +208,11 @@ fn spawn_particles(
     }
 }
 
-fn update_particles(
+// despawn_particles - an update system that will despawn any particles that have outlived
+// their expire-time.
+fn despawn_particles(
     mut commands: Commands,
-    mut query: Query<(Entity, &ExpireTime, &ParticleMarker)>
+    mut query: Query<(Entity, &ExpireTime, &ParticleMarker)>,
 ) {
     // Determine if it's time to despawn particles...if so, do it.
     let now = Instant::now();
